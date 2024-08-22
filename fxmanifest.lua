3c0dee0781e2323d46b1458978feb581b26b542a
BomberTutt <66766059+BomberTutt@users.noreply.github.com>
2024-08-22 16:13:23 -0700

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+
+-----------------For support, scripts, and more----------------
+----------------------Discord - bombertutt---------------------
+---------------------------------------------------------------
+
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Sitting Script'
+version '1.0'
+author 'Tutt'
+
+lua54 'yes'
+
+shared_scripts{
+    '@ox_lib/init.lua',
+    'shared/**/*.lua'
+}
+
+client_scripts{
+    'client/**/*.lua',
+} 
